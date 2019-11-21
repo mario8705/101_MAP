2ccb66deb9a488cba48a56621a6de239f7d12306
Alexis Lavaud <alavaud@student.le-101.fr>
2019-11-21 23:14:24 +0100

    Changed config with new id

--- client/src/config/globalConfig.js
+++ client/src/config/globalConfig.js
@@ -1,7 +1,7 @@
 export default {
-    serverEndpoint: "http://localhost:8080",
-    clientId: "6a70908edc7b210ad935bfeb58ec10870f4f5c9ed4072356aa4a97ea3ff42bf8",
-    redirectUri: "http://localhost:3000/",
+    serverEndpoint: "http://z3r10p1.le-101.fr:8080",
+    clientId: "8dcb6ad9bf8688be7a97d123e8956332c468071307f76958a2a6bb3705181e9f",
+    redirectUri: "http://z3r10p1.le-101.fr:3000/",
     apiEndPoint: "https://api.intra.42.fr",
     defaultTheme: "dark",
     minimalSearchInput: 2,
--- server/config/globalConfig.js
+++ server/config/globalConfig.js
@@ -5,7 +5,7 @@ module.exports = {
     connectedUsers_loopRate: 30, // seconds
     requestsBySecond: 2, // In queue
     connectedUsers_cacheExpiration: 10, // minutes
-    redirect_uri: "http://localhost:3000/",
+    redirect_uri: "http://z3r10p1.le-101.fr:3000/",
     streamLogToConsole: true,
     blocId: 8
 };
